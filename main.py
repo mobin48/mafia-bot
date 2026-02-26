3c09d5a1ec13e1d4252f0c6a0b1f9839cc76c906
mobin48 <volfish18@gmail.com>
2026-02-26 20:36:14 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1130,9 +1130,6 @@ def main():
         print("خطا: توکن ربات تنظیم نشده است!")
         return
 
-     # ساخت اپلیکیشن
-‏    application = Application.builder().token(TOKEN).build()
-
     # اضافه کردن هندلر start
 ‏    application.add_handler(CommandHandler("start", start)) 
     
